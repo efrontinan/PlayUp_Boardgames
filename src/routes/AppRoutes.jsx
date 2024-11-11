d8868356e278272520fa86ea1ad1a659fbe2dd56
Iván Blanco <ivanblanco4m@gmail.com>
2024-11-11 17:45:48 +0100

    routes done

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,16 @@
-
+import { Routes, Route } from "react-router-dom"
 
 const AppRoutes = () => {
     return (
         <div className="AppRoutes">
-            <h1>Pagina de rutas</h1>
+            <Routes>
+                <Route path="/" element={<h1>PRINCPAAAAL</h1>} />
+                <Route path="/juegos" element={<h1>Pagina de juegos</h1>} />
+                <Route path="/juegos/nuevo" element={<h1>Crear juego</h1>} />
+                <Route path="/juegos/editar/:id" element={<h1>Editar juego</h1>} />
+                <Route path="/juegos/detalles/:id" element={<h1>Detalles de juego y crear evento</h1>} />
+                <Route path="/eventos/editar/:id" element={<h1>Editar evento</h1>} />
+            </Routes>
         </div>
     )
 }
