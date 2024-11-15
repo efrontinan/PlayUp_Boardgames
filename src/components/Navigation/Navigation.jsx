50b6952321a9b99e17f1818ebb64d55d508df9ca
efrontinan <elenauxui@gmail.com>
2024-11-15 14:49:03 +0100

    Responsive Navbar details

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,26 +3,28 @@ import { Link, NavLink } from 'react-router-dom'
 
 import './Navigation.css'
 import GlobalGamesFilter from "../../components/GlobalGamesFilter/GlobalGamesFilter"
+import { List } from 'react-bootstrap-icons'
 
 
 const Navigation = () => {
 
     return (
         <div className="Navigation m-5 ">
-            <Navbar  data-bs-theme="dark" className='rounded justify-content-between'>
-                            <Navbar.Brand to="/" as={Link} className='mr-5'> PlayUp</Navbar.Brand>
+            <Navbar data-bs-theme="dark" className='rounded justify-content-between'>
+                <Navbar.Brand to="/" as={Link} className='mr-5'> PlayUp</Navbar.Brand>
 
-                            <Nav className='g24'>
-                                <NavLink to="/juegos"  end className={({ isActive }) => isActive ? "nav-link selected" : "nav-link"} >
-                                    Juegos de mesa
-                                </NavLink>
-                                <NavLink to="/juegos/nuevo" className={({ isActive }) => isActive ? "nav-link selected" : "nav-link"}>
-                                    Nuevo juego
-                                </NavLink>
-                            </Nav>
-                            <Form md="2">
-                                <GlobalGamesFilter />
-                            </Form>
+                <Nav className='g24 d-none d-md-flex'>
+                    <NavLink to="/juegos" end className={({ isActive }) => isActive ? "nav-link selected" : "nav-link"} >
+                        Juegos de mesa
+                    </NavLink>
+                    <NavLink to="/juegos/nuevo" className={({ isActive }) => isActive ? "nav-link selected" : "nav-link"}>
+                        Nuevo juego
+                    </NavLink>
+                </Nav>
+                <Form md="2" className='d-flex flex-row align-items-center'>
+                    <GlobalGamesFilter />
+                </Form>
+                    <List className='d-block d-md-none'/>
             </Navbar>
         </div>
     )
