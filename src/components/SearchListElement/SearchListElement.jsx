60bf29aadc1c6387596f3e5b490bf2d9742ea93f
Iván Blanco <ivanblanco4m@gmail.com>
2024-11-22 10:30:06 +0100

    filter search fix

--- src/components/SearchListElement/SearchListElement.jsx
+++ src/components/SearchListElement/SearchListElement.jsx
@@ -4,18 +4,20 @@ import { ListGroup, Image, Col, Row } from "react-bootstrap"
 
 import './SearchListElement.css'
 
-const SearchListElement = ({ title, image, id, setShowMenu }) => {
+const SearchListElement = ({ title, image, id }) => {
     return (
         <div className="SearchListElement">
             <ListGroup.Item>
-                <Row as={Link} to={`/juegos/detalles/${id}`} onClick={()=> setShowMenu()}>
-                    <Col sm="3" className="d-none d-md-block">
-                        <Image src={image} rounded />
-                    </Col>
-                    <Col sm="9">
-                        <p>{title}</p>
-                    </Col>
-                </Row>
+                <div as={Link} to={`/juegos/detalles/${id}`}>
+                    <Row>
+                        <Col sm="3" className="d-none d-md-block">
+                            <Image src={image} rounded />
+                        </Col>
+                        <Col sm="9">
+                            <p>{title}</p>
+                        </Col>
+                    </Row>
+                </div>
             </ListGroup.Item>
         </div>
     )
